15afa45ffc3003375d3748b6c5d07a54da92c779
Kevin Mulkearn <kevinmulkearn@gmail.com>
2018-01-24 01:50:54 +0000

    ImageView and button test.

--- app/src/main/java/com/mulkearn/kevin/pixelcolor/MainActivity.java
+++ app/src/main/java/com/mulkearn/kevin/pixelcolor/MainActivity.java
@@ -1,13 +1,55 @@
 package com.mulkearn.kevin.pixelcolor;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button searchButton;
+    ImageView imageView;
+    private int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchButton = (Button) findViewById(R.id.searchButton);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        searchButton.setOnClickListener(new View.OnClickListener(){
+           @Override
+            public void onClick(View v){
+               Intent intent = new Intent();
+               intent.setType("image/*");
+               startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_CODE);
+           }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestcode, int resultcode, Intent data){
+        super.onActivityResult(requestcode, resultcode, data);
+
+        if(requestcode == REQUEST_CODE && resultcode == RESULT_OK && data != null && data.getData() != null){
+            Uri uri = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
